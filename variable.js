24aa25264b58b56f3e531b9fd80084ef33848d42
Manu <manojc6363530865@gmail.com>
2025-11-02 20:17:55 +0530

    learnt about oparator

--- variable.js
+++ variable.js
@@ -1,34 +1,59 @@
-let name = "Ilya";
+// let name = "Ilya";
 
-var number = 42;
+// var number = 42;
 
-const boolean = true;
+// const boolean = true;
 
-let undef;
+// let undef;
 
-var nothing = null;
+// var nothing = null;
 
-const symb = Symbol("id");
+// const symb = Symbol("id");
 
-let bigInt = 9007199254741991n;
+// let bigInt = 9007199254741991n;
 
 
-console.log(typeof name);      // "string"
-console.log(typeof number);    // "number"
-console.log(typeof boolean);   // "boolean"
-console.log(typeof undef);     // "undefined"
-console.log(typeof nothing);   // "object"
-console.log(typeof symb);      // "symbol"
-console.log(typeof bigInt);    // "bigint"
+// console.log(typeof name);      // "string"
+// console.log(typeof number);    // "number"
+// console.log(typeof boolean);   // "boolean"
+// console.log(typeof undef);     // "undefined"
+// console.log(typeof nothing);   // "object"
+// console.log(typeof symb);      // "symbol"
+// console.log(typeof bigInt);    // "bigint"
 
-let print = document.createElement("div");
-print.innerHTML = `
-  <p>name: ${name}, type: ${typeof name}</p>
-  <p>number: ${number}, type: ${typeof number}</p>
-  <p>boolean: ${boolean}, type: ${typeof boolean}</p>
-  <p>undef: ${undef}, type: ${typeof undef}</p>
-  <p>nothing: ${nothing}, type: ${typeof nothing}</p>
-  <p>symb: ${symb.toString()}, type: ${typeof symb}</p>
-  <p>bigInt: ${bigInt}, type: ${typeof bigInt}</p>
-`;
-document.body.appendChild(print);
+// // let print = document.createElement("div");
+// // print.innerHTML = `
+// //   <p>name: ${name}, type: ${typeof name}</p>
+// //   <p>number: ${number}, type: ${typeof number}</p>
+// //   <p>boolean: ${boolean}, type: ${typeof boolean}</p>
+// //   <p>undef: ${undef}, type: ${typeof undef}</p>
+// //   <p>nothing: ${nothing}, type: ${typeof nothing}</p>
+// //   <p>symb: ${symb.toString()}, type: ${typeof symb}</p>
+// //   <p>bigInt: ${bigInt}, type: ${typeof bigInt}</p>
+// // `;
+// // document.body.appendChild(print);
+
+// // promt, alert and confirm
+// let userName = prompt("What is your name?", "Ilya");
+// alert(`Hello, ${userName}!`);
+
+// let isAdult = confirm("Are you 18 or older?");
+// if (isAdult) {
+//   alert("Access granted.");
+// } else {
+//   alert("Access denied.");
+// }
+
+
+// // Template literals
+// let a = 5;
+// let b = 10;
+// console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
+
+
+/*const isEven = 2 % 2 ===0 ? "even" : "odd";
+console.log(`The number 2 is ${isEven}.`);*/
+
+// Compression oparator
+
+console.log("daniel"=="ieandl")
